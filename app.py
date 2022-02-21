435d94a4cd0b7265aba7a102026350ecaa570199
Parvez09 <58951926+Parvez09@users.noreply.github.com>
2022-02-21 23:33:45 +0530

    Sentiment-Classification-Deployement-of-ML-model-using-Flask-WebAPP
    Deploying your machine learning model project Using Flask-web App
    
    Learn how to use Flask to deploy a machine learning model into production

---
+++ app.py
@@ -0,0 +1,50 @@
+from flask import Flask, request, render_template
+import requests
+import sklearn
+import pickle
+import pandas as pd
+
+app = Flask(__name__)
+model = pickle.load(open("sentiment.pkl", "rb"))
+vector = pickle.load(open("vector.pkl", "rb"))
+
+@app.route('/',methods=['GET'])
+def Home():
+    return render_template('index.html')
+
+@app.route('/predict',methods=['POST'])
+def predict():
+    if request.method == 'POST':
+        text = request.form["text"]
+
+        prediction = model.predict(vector.transform([text]))
+        if prediction ==1:
+
+            return render_template('index.html',prediction_text="Your Review is Positive")
+        else:
+            return render_template('index.html',prediction_text="Your Review is Negative")
+
+    else:
+        return render_template('index.html')
+
+if __name__=="__main__":
+    app.run(debug=True)
+
+
+
+
+
+
+
+
+            
+
+        
+        
+        
+
+
+
+
+    
+
